0f7f0ad1c51d36f64e57d2a3ddf2188aac45c741
Ma, Sang-gil <masanggil1986@gmail.com>
2017-10-16 13:04:08 +0800

    Initial Commit

---
+++ functions/README.md
@@ -0,0 +1,3 @@
+# Congregation Assistant Trigger
+
+All database triggers and schduled tasks will be handled by this project.
---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const CryptoJS = require('crypto-js');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.createCongregation = functions.https.onRequest((req, res) => {
+    const congregationName = req.query.congregation;
+    if (congregationName) {
+        const user = {
+            username: 'admin',
+            password: CryptoJS.MD5('admin').toString(),
+            name: 'Administrator',
+            role: 'admin'
+        };
+        admin.database().ref(`${congregationName}/users`).push(user).then(snapshot => {
+            res.status(200).send(`${congregationName} has been created!`);
+        });
+    } else {
+        res.status(400).send(`No congregation provided!`);
+    }
+});
